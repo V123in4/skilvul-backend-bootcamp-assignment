46ff391ae0acc6c6bbffdd37142a7b8d9835dcac
V123in4 <vinconloren@gmail.com>
2022-10-10 15:01:41 +0700

    fixed images

--- TPA003/script.js
+++ TPA003/script.js
@@ -32,12 +32,19 @@ let search = async (query) => {
     contentContainer.innerHTML = ``;
 
     data.results.forEach(data => {
-        console.log(data.poster_path);
+        let img_path;
+        if (data.poster_path == null)
+        {
+            img_path = "./no-img-place-holder.jpg";
+        } else {
+            img_path = `https://image.tmdb.org/t/p/original/${data.poster_path}" alt="image not available`;
+        }
+
         contentContainer.innerHTML += 
         `
         <div>
             <figure>
-                <img src="https://image.tmdb.org/t/p/original/${data.poster_path}" alt="image not aviable" />
+                <img src="${img_path}" />
                 <figcaption>
                     <span class="movie-title">${data.title}</span
                     ><b class="rating">${data.vote_average}</b>
